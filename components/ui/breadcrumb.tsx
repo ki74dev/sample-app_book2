798f52b10984d91f8c88ebb17c9585781df03964
User <user@gmail.com>
2024-08-28 13:28:29 +0900

    パンくずリストのリンク修正

--- components/ui/breadcrumb.tsx
+++ components/ui/breadcrumb.tsx
@@ -3,6 +3,7 @@ import { ChevronRightIcon, DotsHorizontalIcon } from "@radix-ui/react-icons"
 import { Slot } from "@radix-ui/react-slot"
 
 import { cn } from "@/lib/utils"
+import Link from "next/link"
 
 const Breadcrumb = React.forwardRef<
   HTMLElement,
@@ -41,11 +42,11 @@ BreadcrumbItem.displayName = "BreadcrumbItem"
 
 const BreadcrumbLink = React.forwardRef<
   HTMLAnchorElement,
-  React.ComponentPropsWithoutRef<"a"> & {
+  React.ComponentPropsWithoutRef<typeof Link> & {
     asChild?: boolean
   }
 >(({ asChild, className, ...props }, ref) => {
-  const Comp = asChild ? Slot : "a"
+  const Comp = asChild ? Slot : Link
 
   return (
     <Comp
